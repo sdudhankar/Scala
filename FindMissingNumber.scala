7c6b9743e7b49432e9e4f95e4078f617d8418b91
Sachin Dudhankar <dudhankarsachin16@gmail.com>
2020-03-01 21:31:07 +0530

    Update FindMissingNumber.scala

--- FindMissingNumber.scala
+++ FindMissingNumber.scala
@@ -1,1 +1,14 @@
-
+object FindMissingNumber {
+   def main(args: Array[String]) {
+      val a = Array[Int](1,2,3,4,6)
+      println("Missing number in Array: "+missingNumber(a))
+     
+      def missingNumber(a:Array[Int]):Int={
+         var sum = (a.length+1)*(a.length+2)/2
+         println(sum)
+         for(i<-0 to a.length-1)
+            sum=sum-a(i)
+        sum    
+     }
+   }
+}
